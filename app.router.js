8c905bfde3f7df62c9e4d5a0e0ebe625614031c0
Rufaidah Jomaa <161156699+rufaidah-jomaa@users.noreply.github.com>
2024-05-01 00:53:51 +0300

    Update app.router.js

--- app.router.js
+++ app.router.js
@@ -3,9 +3,11 @@ import messageRouter from './src/modules/message/message.router.js'
 import authRouter from './src/modules/auth/auth.router.js'
 import userRouter from './src/modules/user/user.router.js'
 export const initApp=(app,express)=>{
-
-    app.use(express.json());
-   connectDB();
+     connectDB();
+     app.use(express.json());
+     app.get("/", (req, res) => {
+      return res.status(200).json("welcome");
+      });
     app.use('/message',messageRouter);
     app.use('/auth',authRouter);
     app.use('/user',userRouter)
